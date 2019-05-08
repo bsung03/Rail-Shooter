9618b28b929b3b1185d4c4a30e626af9481193d1
Brandon Sung <bsung@purdue.edu>
2019-05-08 18:20:31 -0400

    First commit

---
+++ Assets/Scipts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    private CharacterController character_Controller;
+    private Vector3 move_Direction;
+    public float speed = 5f;
+    private float gravity = 20f;
+    public float jump_Force = 10f;
+    private float vertacal_Velocity;
+
+
+    void Start(){
+        character_Controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movePlayer();
+    }
+
+    private void movePlayer()
+    {
+        move_Direction = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+        move_Direction = transform.TransformDirection(move_Direction);
+        move_Direction *= speed * Time.deltaTime;
+
+        character_Controller.Move(move_Direction);
+    }
+}
